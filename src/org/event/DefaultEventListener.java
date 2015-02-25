d14acde62dfb520d85d450444ffbc0592da08449
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 12:22:27 +0530

    default event listener now reports error

--- src/org/event/DefaultEventListener.java
+++ src/org/event/DefaultEventListener.java
@@ -8,8 +8,15 @@ import java.util.*;
 
 public class DefaultEventListener implements IEventListener {
 
+    // Listen (event, args)
+    // - print event details and exit on error
     @Override
+    @SuppressWarnings("CallToPrintStackTrace")
     public void listen(String event, Map args) {
         System.out.println("["+event+"] : "+args);
+        if(!args.containsKey("err")) return;
+        Throwable err = (Throwable)args.get("err");
+        err.printStackTrace();
+        System.exit(-1);
     }
 }
