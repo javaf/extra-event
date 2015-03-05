5e3d5ea054300ba98a6e3deae3bf6afc4eb5e493
Subhajit Sahu <wolfram77@gmail.com>
2015-03-06 01:20:46 +0530

    ready to absorb?

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,16 +7,6 @@ package main;
 
 public class Main {
     
-    public static void print(Object... str) {
-        for(Object item : str)
-            System.out.println(item);
-    }
-    
-    public static void print2(Object... str) {
-        print(str);
-    }
-    
     public static void main(String[] args) throws Throwable {
-        print2("a", "b");
     }
 }
