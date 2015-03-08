040bf552813afef744bbd721b606fd4aa101972c
Subhajit Sahu <wolfram77@gmail.com>
2015-03-08 23:18:44 +0530

    used concurrent hashmap

--- src/org/event/EventEmitter.java
+++ src/org/event/EventEmitter.java
@@ -4,71 +4,39 @@ package org.event;
 // required modules
 import org.data.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 
 
-public class EventEmitter extends HashMap<String, Set<IEventAbsorber>> {
+public class EventEmitter extends ConcurrentHashMap<String, Set<IEventAbsorber>> {
     
-    // data
-    IEventAbsorber fallback;
-    
-    
-    // Emit (event, args)
-    // - emit a event to all absorbers with specified fallback
-    void emit(IEventAbsorber fb, String event, Map args) {
-        Set<IEventAbsorber> absb = get(event);
-        if(absb == null) { if(fb != null) fb.absorb(event, args); return; }
-        for(IEventAbsorber e : absb)
-            e.absorb(event, args);
-    }
-    
-    
-    // EventEmitter ()
-    // - create an event emitter
-    public EventEmitter() {
-        fallback = new DefaultEventAbsorber();
-    }
-    
-    
-    // Fallback ()
-    // - get fallback event absorber
-    public IEventAbsorber fallback() {
-       return fallback; 
-    }
-    
-    
-    // Fallback (e)
-    // - set fallback event absorber
-    public EventEmitter fallback(IEventAbsorber e) {
-        fallback = e;
-        return this;
-    }
+    // static data
+    public static IEventAbsorber fallback = new DefaultEventAbsorber();
     
     
     // Emit (event, args)
     // - emit a event to all absorbers
     public EventEmitter emit(String event, Map args) {
-        emit(fallback, event, args);
+        Set<IEventAbsorber> absorbers = get(event);
+        if(absorbers == null) fallback.absorb(event, args);
+        else absorbers.stream().forEach((e) -> {
+            e.absorb(event, args);
+        });
         return this;
     }
     
     
-    // Emit (e, event, args...)
+    // Emit (event, args...)
     // - emit a event to all absorbers
     public EventEmitter emit(String event, Object... args) {
         return emit(event, Coll.map(args));
     }
     
     
-    public Set<IEventAbsorber> put(String event, Set<IEventAbsorber> absorber) {
-        return null;
-    }
-    
-    
     // Add (event, absorber)
     // - add an absorber to an event
     public EventEmitter add(String event, IEventAbsorber absorber) {
-        if(get(event) == null) put(event, new HashSet<>());
+        if(get(event) == null) put(event, Collections.newSetFromMap(new ConcurrentHashMap<IEventAbsorber, Boolean>()));
         get(event).add(absorber);
         return this;
     }
