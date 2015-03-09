c2aa0b98280d58422c7de58e981096ad8f4f999e
Subhajit Sahu <wolfram77@gmail.com>
2015-03-10 00:13:41 +0530

    removed lambda optimize

--- src/org/event/Eventer.java
+++ src/org/event/Eventer.java
@@ -5,7 +5,6 @@ package org.event;
 import java.util.*;
 import java.lang.invoke.*;
 import java.lang.reflect.*;
-import java.util.function.*;
 
 
 
@@ -14,7 +13,6 @@ public class Eventer implements Eventable {
     // data
     Object obj;
     MethodHandle mthd;
-    Eventable lambda;
     
     
     // Eventer (cls, mthd)
@@ -23,10 +21,9 @@ public class Eventer implements Eventable {
     public Eventer(Class cls, String mthd) {
         try {
             Method m = cls.getMethod(mthd, String.class, Map.class);
-            MethodHandle mh = MethodHandles.lookup().unreflect(m);
-            lambda = (Eventable)LambdaMetafactory.metafactory(MethodHandles.lookup(), "absorb", MethodType.methodType(Eventable.class), mh.type(), mh, mh.type()).getTarget().invokeExact();
+            this.mthd = MethodHandles.lookup().unreflect(m);
         }
-        catch(Throwable e) { new EventException(e).exit(); }
+        catch(Exception e) { new EventException(e).exit(); }
     }
     
     
@@ -43,10 +40,8 @@ public class Eventer implements Eventable {
     @Override
     public void absorb(String event, Map args) {
         try {
-            if(obj == null) lambda.absorb(event, args);
-            else lambda.absorb(event, args);
-            // if(obj == null) mthd.invokeExact(event, args);
-            // else mthd.invoke(obj, event, args);
+            if(obj == null) mthd.invokeExact(event, args);
+            else mthd.invoke(obj, event, args);
         }
         catch(Throwable e) { new EventException(e).exit(); }
     }
