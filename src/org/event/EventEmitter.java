295b1459b8025d6b9d818fbc9aa0637f8f6a8d3b
Subhajit Sahu <wolfram77@gmail.com>
2015-03-09 19:06:10 +0530

    basic eventer ready

--- src/org/event/EventEmitter.java
+++ src/org/event/EventEmitter.java
@@ -14,6 +14,14 @@ public class EventEmitter extends ConcurrentHashMap<String, Set<Eventable>> {
     public static Eventable fallback = new DefEventer();
     
     
+    // _InitEventSet (event)
+    // - initialize event set before adding
+    void _initEventSet(String event) {
+        if(get(event) != null) return;
+        put(event, Collections.newSetFromMap(new ConcurrentHashMap<Eventable, Boolean>()));
+    }
+    
+    
     // Emit (event, args)
     // - emit a event to all eventers
     public EventEmitter emit(String event, Map args) {
@@ -33,35 +41,54 @@ public class EventEmitter extends ConcurrentHashMap<String, Set<Eventable>> {
     }
     
     
-    // Add (event, eventer)
+    // On (event, eventer)
     // - add an eventer to an event
-    public EventEmitter add(String event, Eventable absorber) {
-        if(get(event) == null) put(event, Collections.newSetFromMap(new ConcurrentHashMap<Eventable, Boolean>()));
-        get(event).add(absorber);
+    public EventEmitter on(String event, Eventer eventer) {
+        _initEventSet(event);
+        get(event).add(eventer);
+        return this;
+    }
+    
+    
+    // On (event, eventers)
+    // - add eventers to an event
+    public EventEmitter on(String event, Collection<Eventer> eventers) {
+        _initEventSet(event);
+        get(event).addAll(eventers);
+        return this;
+    }
+    
+    
+    // On (events, eventer)
+    // - add eventer to events
+    public EventEmitter on(Collection<String> events, Eventer eventer) {
+        events.stream().forEach((event) -> {
+            on(event, eventer);
+        });
         return this;
     }
 
     
-    // Remove (event, eventer)
+    // NotOn (event, eventer)
     // - remove an eventer from an event
-    public EventEmitter remove(String event, Eventable absorber) {
+    public EventEmitter notOn(String event, Eventable eventer) {
         Set<Eventable> absb = get(event);
-        if(absb != null) absb.remove(absorber);
+        if(absb != null) absb.remove(eventer);
         return this;
     }
     
     
-    // Remove (event)
-    // - remove all eventer of an event
-    public EventEmitter remove(String event) {
-        super.remove(event);
+    // NotOn (event)
+    // - remove all eventers of an event
+    public EventEmitter notOn(String event) {
+        remove(event);
         return this;
     }
     
     
-    // Remove ()
-    // - remove all event eventer
-    public EventEmitter remove() {
+    // notOn ()
+    // - remove all eventers
+    public EventEmitter notOn() {
         clear();
         return this;
     }
