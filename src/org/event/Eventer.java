3e272bfde51ac2fdb3b81edf33cc1309f02c3664
Subhajit Sahu <wolfram77@gmail.com>
2015-03-09 18:44:00 +0530

    eventer added

---
+++ src/org/event/Eventer.java
@@ -0,0 +1,48 @@
+// @wolfram77
+package org.event;
+
+// required modules
+import java.util.*;
+import java.lang.invoke.*;
+import java.lang.reflect.*;
+
+
+
+public class Eventer implements Eventable {
+    
+    // data
+    Object obj;
+    MethodHandle mthd;
+    
+    
+    // Eventer (cls, mthd)
+    // - create an eventer (static)
+    @SuppressWarnings("UseSpecificCatch")
+    public Eventer(Class cls, String mthd) {
+        try {
+            Method m = cls.getMethod(mthd, String.class, Map.class);
+            this.mthd = MethodHandles.lookup().unreflect(m);
+        }
+        catch(Exception e) { new EventException(e).exit(); }
+    }
+    
+    
+    // Eventer (obj, mthd)
+    // - create an eventer (instance)
+    public Eventer(Object obj, String mthd) {
+        this(obj.getClass(), mthd);
+        this.obj = obj;
+    }
+    
+    
+    // Absorb (event, args)
+    // - absorbs an event and forwards it to method
+    @Override
+    public void absorb(String event, Map args) {
+        try {
+            if(obj == null) mthd.invokeExact(event, args);
+            else mthd.invoke(obj, event, args);
+        }
+        catch(Throwable e) { new EventException(e).exit(); }
+    }
+}
