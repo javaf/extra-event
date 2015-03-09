6b741dc9dfd3371b7418818d6cebf6fb3c8a0769
Subhajit Sahu <wolfram77@gmail.com>
2015-03-09 23:46:23 +0530

    tests using method handle

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,16 +10,24 @@ import org.event.*;
 public class Main {
     
     public static void onMouseClick(String event, Map args) {
-        System.out.println("mouse clicked");
     }
     
     public void onKeyPress(String event, Map args) {
     }
     
     public static void main(String[] args) {
+        int N = 1000000;
         EventEmitter event = new EventEmitter(Main.class);
         System.out.println(event);
-        event.emit("mouse-click");
-        event.emit("key-press");
+        long t0 = System.nanoTime();
+        for(int i=0; i<N; i++)
+            event.emit("mouse-click");
+        long t1 = System.nanoTime();
+        for(int i=0; i<N; i++)
+            onMouseClick(null, null);
+        long t2 = System.nanoTime();
+        double tmh = (t1-t0)/Math.pow(10, 9);
+        double ti = (t2-t1)/Math.pow(10, 9);
+        System.out.println("MH = "+tmh+", I = "+ti);
     }
 }
