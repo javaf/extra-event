594adae45133c6090d560a74e548d9d86273fd75
Subhajit Sahu <wolfram77@gmail.com>
2015-03-11 01:03:32 +0530

    React annotation ready with javadoc

--- src/org/event/React.java
+++ src/org/event/React.java
@@ -1,10 +1,22 @@
 // @wolfram77
 package org.event;
 
+// required modules
+import java.lang.annotation.*;
+
+
+
 /**
- *
+ * React annotation for specifying reaction mode
  * @author wolfram77
  */
-public interface React {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface React {
     
+    /**
+     * Reaction mode: "sync" or "async"
+     * @return reaction mode
+     */
+    public String mode() default "sync";
 }
