79ee78d45b05a11b6696986aefb8662658c572cd
Subhajit Sahu <wolfram77@gmail.com>
2015-03-12 14:02:53 +0530

    Reactable doc updated

--- src/org/event/Reactable.java
+++ src/org/event/Reactable.java
@@ -7,13 +7,14 @@ import java.util.*;
 
 
 /**
- * <h3>Interface that can be reacted on a stimulus</h3>
+ * <h3>Interface that can react on a stimulus</h3>
+ * Create a class implementing this interface to be able to react to a stimulus
  * @author wolfram77
  */
 public interface Reactable {
     
     /**
-     * <h3>Reacts on a stimulus</h3>
+     * <h3>React on a stimulus</h3>
      * @param stimulus name of stimulus
      * @param args additional arguments
      */
