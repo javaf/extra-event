365be46fe7031a61b2f51dbaf8362f416be1c0e2
Subhajit Sahu <wolfram77@gmail.com>
2015-03-17 14:49:04 +0530

    testing reactable class example

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,38 @@
 package main;
 
 // required modules
+import java.util.*;
 import org.event.*;
+
+class HelloReactor implements Reactable {
+
+    @Override
+    public void on(String stimulus, Map args) {
+        System.out.println("Lets get to work");
+    }
+}
+
+class ByeReactor implements Reactable {
+
+    @Override
+    @Reacts("slow")
+    public void on(String stimulus, Map args) {
+        System.out.print("Name: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println("Nice to meet you "+name);
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
+        HelloReactor helloReaction = new HelloReactor();
+        // annotations only work in Reaction objects
+        Reaction byeReaction = new Reaction(new ByeReactor());
         Spine spine = new Spine();
-        try { throw new RuntimeException("Got a Sprain"); }
-        catch(Exception e) { spine.is("injury", "err", e, "msg", "Cant go to school"); }
-        // err argument indicates it is an error stimulus
+        // chaining method calls is supported
+        spine.on("hello", helloReaction).on("bye", byeReaction);
+        spine.is("hello").is("bye");
     }
 }
