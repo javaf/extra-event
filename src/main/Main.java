356346e0df27c18ffcf00b0acd245a98995ffe77
Subhajit Sahu <wolfram77@gmail.com>
2015-03-17 17:33:35 +0530

    Reaction Method example tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,19 +2,31 @@
 package main;
 
 // required modules
+import java.util.*;
 import org.event.*;
-
-
 
 public class Main {
 
+    public static void helloReactor(String stimulus, Map args) {
+        System.out.println("Lets get to work");
+    }
+
+    public void byeReactor(String stimulus, Map args) {
+        System.out.print("Name: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println("Nice to meet you "+name);
+    }
+
     public static void main(String[] args) {
-        HelloReactor helloReaction = new HelloReactor();
-        // annotations only work in Reaction objects
-        Reaction byeReaction = new Reaction(new ByeReactor());
+        Main main = new Main();
         Spine spine = new Spine();
-        // chaining method calls is supported
-        spine.on("hello", helloReaction).on("bye", byeReaction);
-        spine.is("hello").is("bye");
+        // static reaction method
+        spine.on("hello", new Reaction(Main.class, "helloReactor"));
+        // instance reaction method 
+        // speed can be indicated manually as well
+        spine.on("bye", new Reaction(main, "byeReactor").speed("slow"));
+        spine.is("hello");
+        spine.is("bye");
     }
 }
