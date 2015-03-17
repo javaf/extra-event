717315a01a2a156abcb0d263dc406e0a6e1628b6
Subhajit Sahu <wolfram77@gmail.com>
2015-03-17 19:17:23 +0530

    Event Loop example test

--- src/main/Introducer.java
+++ src/main/Introducer.java
@@ -1,19 +1,47 @@
-// package
+// @wolfram77
 package main;
 
 // required modules
+import java.util.concurrent.*;
 import java.util.*;
+import org.event.*;
 
-public class Introducer {
+class Introducer extends Thread implements Reactable {
 
-    public static void onHello(String stimulus, Map args) {
-        System.out.println("Lets get to work");
+    public Spine spine;
+    BlockingQueue<Object[]> events;
+
+    public Introducer() {
+    	spine = new Spine(this);
+    	events = new LinkedBlockingQueue<>();
     }
-
+    
+    public static void onHello(String stimulus, Map args) {
+    	System.out.println("Lets get to work");
+    }
+    
+    @Reacts("slow")
     public void onBye(String stimulus, Map args) {
         System.out.print("Name: ");
         Scanner in = new Scanner(System.in);
         String name = in.next();
         System.out.println("Nice to meet you "+name);
+    }
+    
+    @Override
+    public void run() {
+        try {
+            while(true) {
+                Object[] o = events.take();
+                spine.is((String)o[0], (Map)o[1]);
+            }
+        }
+        catch(InterruptedException e) {}
+    }
+    
+    @Override
+    public void on(String stimulus, Map args) {
+        try { events.put(new Object[]{stimulus, args}); }
+        catch(InterruptedException e) {}
     }
 }
