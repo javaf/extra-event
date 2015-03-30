647e9339adb3fc9032c19f615152aede0b75637f
Subhajit Sahu <wolfram77@gmail.com>
2015-03-30 22:35:29 +0530

    Added FunctionalInterface annotation to Reactable

--- src/org/event/Reactable.java
+++ src/org/event/Reactable.java
@@ -11,6 +11,7 @@ import java.util.*;
  * <div>Classes implementing this interface are able to react to stimuli</div>
  * @author wolfram77
  */
+@FunctionalInterface
 public interface Reactable {
     
     /**
