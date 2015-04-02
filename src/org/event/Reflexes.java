70b60c6988bac3ada4be0f8f58ad78b0f529fd0f
Subhajit Sahu <wolfram77@gmail.com>
2015-04-02 16:49:20 +0530

    Reacts -> Reflexes with refactoring

--- src/org/event/Reflexes.java
+++ src/org/event/Reflexes.java
@@ -8,13 +8,13 @@ import java.lang.annotation.*;
 
 /**
  * <b>Annotation to specify reaction-method speed</b>
- * <div>Use {@code @Reacts("fast")} for fast reaction-method (optional)</div>
- * <div>Use {@code @Reacts("slow")} for slow reaction-method (required)</div>
+ * <div>Use {@code @Reflexes("fast")} for fast reaction-method (optional)</div>
+ * <div>Use {@code @Reflexes("slow")} for slow reaction-method (required)</div>
  * @author wolfram77
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Reacts {
+public @interface Reflexes {
     
     /**
      * <b>Speed of reaction-method ("fast" or "slow")</b>
