810b7c18a49292a09956dbfeb698cf5e42e651ce
Subhajit Sahu <wolfram77@gmail.com>
2015-04-04 17:14:00 +0530

    prebuild file added

---
+++ prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir lib
+wget -O lib/junit-4.12.jar "http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar"
+wget -O lib/hamcrest-core-1.3.jar "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
