a3ed4e110a1825c3b6739da11d466d3fbf0bb3cb
Subhajit Sahu <wolfram77@gmail.com>
2015-04-05 17:13:05 +0530

    incorrect pathname

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir .ant
-mkdir .ant/lib
+mkdir -p .ant/lib
 wget -O .ant/lib/junit-4.12.jar "http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar"
-wget -O lib/hamcrest-core-1.3.jar "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
+wget -O .ant/lib/hamcrest-core-1.3.jar "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
