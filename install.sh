0391496f380621a1ff0aa2bce65e0b88f4a207c1
Subhajit Sahu <wolfram77@gmail.com>
2015-04-05 17:15:43 +0530

    library files of junit added

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p .ant/lib
-wget -O .ant/lib/junit-4.12.jar "http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar"
-wget -O .ant/lib/hamcrest-core-1.3.jar "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
